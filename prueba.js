f3fffe40fc6bbe5ddcccab294c5799fe458a73d8
prgramxf23 <fh1273509@mail.com>
2024-01-01 23:03:21 -0300

    tipo de datos y asignaciones

--- prueba.js
+++ prueba.js
@@ -2,9 +2,14 @@ var nombre = "Fernando";
 var edad = 52;
 var presenteono = "present";
 var apellido = "Herrera";
+var peso = "70";
+
 
 
 //variables: var, let y const
+//en el caso del peso, el valor de la varible entre comillas representa un string, ya que no le asignamos el tipo de dato
 
 
-alert("Hola JS" + " " + nombre + " " + edad + " " + apellido);
+//alert("Hola JS" + " " + nombre + " " + edad + " " + apellido);
+
+console.log("nombre:" + nombre + " " + peso + typeof peso + " " + edad + typeof edad );

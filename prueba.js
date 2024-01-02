cc35385b5cc460ff582ec5d8ac3a53f201c67ce2
prgramxf23 <fh1273509@mail.com>
2024-01-01 22:33:52 -0300

    algo de variables

--- prueba.js
+++ prueba.js
@@ -1,1 +1,10 @@
-alert("Hola JS");
+var nombre = "Fernando";
+var edad = 52;
+var presenteono = "present";
+var apellido = "Herrera";
+
+
+//variables: var, let y const
+
+
+alert("Hola JS" + " " + nombre + " " + edad + " " + apellido);

8658a85ba561016b3e4ec8ea9ab0f068864645b0
programxf23 <fh1273509@gmail.com>
2024-01-03 19:53:32 -0300

    ejercicio condicional integrador

--- prueba.js
+++ prueba.js
@@ -1,22 +1,38 @@
-//condicional switch
+//ejercicio integrador
 
-var color = prompt("Ingresa un color");
+/*Ingresar dos numeros mediante un promt y almacenar sus valores
+en dos variables distintas.
+Realizar una resta entre los dos valores en js
+Almacenar el resultado en una variable
+Importar el archivo js desde una pag html
+Si el resultado de la resta es mayor a 0, mostrar mediante un console.log
+o un alert el mensaje "es mayor a 0"
+Si es mayor a 0, comprarar si el numero es par, en caso de que lo
+sea, mostrar un mensaje "es par y en caso de que no, uno que diga
+"es impar"
+En caso de que sea menor a 0, mostrar un mensaje que diga
+es menor o igual a 0.
+*/
 
-switch (color) {
+var numero1 = prompt("ingrese un numero");
+var numero2 = prompt("ingrese otro numero");
 
-    case "rojo":
-        console.log("Rojo");
-        break;
+var resta = numero1 - numero2;
 
-    case "azul":
-        console.log("Azul");
-        break;
+if (resta > 0) {
+  console.log("es mayor a 0");
+    let impar = resta % 2;
 
-    case "verde":
-        console.log("Verde");
-        break;
+ if (impar == 0) {
+  console.log("es par");
+ 
 
-    default:
-        console.log("Color no valido");
-        break;
+} 
+else {
+  console.log("es impar");
+} 
 }
+else {
+    console.log("es menor a 0");
+}
+
